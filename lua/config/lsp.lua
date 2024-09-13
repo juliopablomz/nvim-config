ae0d14ba8d4a27d00f034e5d1c53ee0737ab8a09
Júlio Pablo <juliopablomuniz@hotmail.com>
2024-09-13 18:18:59 -0300

    add: better lsp configuration.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,11 +2,25 @@ local lspconfig = require('lspconfig');
 local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- configure you installed lsp here:
-local servers = { "clangd" };
+local servers = { 
+  { 
+    name = 'clangd', 
+    -- cmd field is optional:
+    cmd = {
+      'clangd',
+      '-header-insertion=never',
+    },
+  },
+};
 
 for _, server in ipairs(servers) do
-  lspconfig[server].setup({
+  local config = {
     capabilities = cmp_capabilities,
-  })
-end
+  }
 
+  if server.cmd then
+    config.cmd = server.cmd
+  end
+
+  lspconfig[server.name].setup(config)
+end
